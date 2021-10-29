18838239a0379689719b3cd0d18425144a36056a
maxherget <hergetmax@icloud.com>
2021-10-29 14:22:12 +0200

    Einrichtung

---
+++ app/src/main/java/htw/berlin/wi/prog2/domain/PrecomputedBowl.java
@@ -0,0 +1,16 @@
+package htw.berlin.wi.prog2.domain;
+
+import java.util.List;
+
+public class PrecomputedBowl implements Bowl {
+   public double calculatePrice(){
+   return;
+    }
+   public int calculateCalories(){
+   return;
+    }
+    public List<String> getIngredientNames(){
+    return;
+    }
+
+}
